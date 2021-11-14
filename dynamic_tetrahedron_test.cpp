41dc2c45fa5c397652ab8a21035b02d0693423b0
mqrieck <mrieck@drake.edu>
2021-11-14 09:43:36 -0600

    Update dynamic_tetrahedron_test.cpp

--- dynamic_tetrahedron_test.cpp
+++ dynamic_tetrahedron_test.cpp
@@ -1,5 +1,5 @@
 
-// dynamic_tetrahedon_test.cpp (by M. Q. Rieck, updated: 11/9/2021)
+// dynamic_tetrahedon_test.cpp (by M. Q. Rieck, updated: 11/14/2021)
 
 // Note: This is test code for the results in my "tetrahedron and toroids" paper, and more.
 
@@ -22,13 +22,16 @@
 #include <cstdlib>
 #include <ncurses.h>
 
-#define M 1600                  // how many (alpha, beta, gamma) points (M^3)?
-#define N 100                   // how fine to subdivide the interval [0, pi]
+#define M 800                   // how many (alpha, beta, gamma) points (M^3)?
+#define N 70                    // how fine to subdivide the interval [0, pi]
 #define O 0                     // set higher to avoid low "tilt planes"
 #define pi M_PI                 // pi = 3.141592654..., of course
-#define ACUTE_TEST              // only appropriate for acute base triangle ABC
-#define COSINES_TEST            // include the "cosines test" when using an acute triangle
-#define NEW_TESTS		// use the new tests based on Grunert's system discriminant
+//#define EXTRA_RULES_1         // some extra tests that could be superfluous
+//#define EXTRA_RULES_2         // some more such extra tests
+#define ACUTE_TESTING           // only appropriate for acute base triangle ABC
+#define MAX_RULES               // testing based on toroid analysis
+#define EASY_COSINE_RULES       // more testing based of toroid analysis
+#define GRUNERT_DISCR_RULE      // use the new tests based on Grunert's system discriminant
 #define REFINED                 // more refined testing for accepting a cell
 //#define SHOW_EXTRA            // display a couple significant regions
 #define STARTX 2                // horizontal start of displayed character grid
@@ -157,14 +160,21 @@ int main(int argc, char **argv) {
                 alpha +  beta + gamma < 2*pi &&
                 alpha <  beta + gamma &&
                 beta  < gamma + alpha &&
-                gamma < alpha +  beta &&
+                gamma < alpha +  beta
+#ifdef EXTRA_RULES_1
+                &&
                 A + beta + gamma  < 2*pi &&
                 alpha + B + gamma < 2*pi &&
-                alpha + beta + C  < 2*pi &&
+                alpha + beta + C  < 2*pi
+#endif
+#ifdef EXTRA_RULES_2
+                &&
                 beta  + gamma - alpha < 2*(B+C) &&
                 gamma + alpha -  beta < 2*(C+A) &&
                 alpha +  beta - gamma < 2*(A+B)
-#ifdef ACUTE_TEST
+#endif
+#ifdef ACUTE_TESTING
+#ifdef MAX_RULES
                 &&
                 (alpha >= A || beta  < B || beta  < C + alpha) &&
                 (alpha >= A || gamma < C || gamma < B + alpha) &&
@@ -172,14 +182,15 @@ int main(int argc, char **argv) {
                 (beta  >= B || alpha < A || alpha < C + beta ) &&
                 (gamma >= C || alpha < A || alpha < B + gamma) &&
                 (gamma >= C || beta  < B || beta  < A + gamma)
-#ifdef COSINES_TEST
+#endif
+#ifdef EASY_COSINE_RULES
                 &&
                 (alpha >= A || cosC * cos_beta  + cosB * cos_gamma > 0) &&
                 (beta  >= B || cosA * cos_gamma + cosC * cos_alpha > 0) &&
                 (gamma >= C || cosB * cos_alpha + cosA * cos_beta  > 0)
 #endif
-#ifdef NEW_TESTS
-                && // if outside CSDC then cannot be inside exactly two basic toroids!
+#ifdef GRUNERT_DISCR_RULE
+                && // if outside CSDC then cannot be inside exactly two basic toroids
                 ( D < 0 || (
                   (alpha >= A || beta <  B || gamma <  C) &&
                   (alpha <  A || beta >= B || gamma <  C) &&
