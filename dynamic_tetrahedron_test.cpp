8b5a0d5610b09a008fa4ec769334661191943edf
mqrieck <mrieck@drake.edu>
2022-06-11 10:44:19 -0500

    Update dynamic_tetrahedron_test.cpp

--- dynamic_tetrahedron_test.cpp
+++ dynamic_tetrahedron_test.cpp
@@ -148,8 +148,8 @@ int main(int argc, char **argv) {
   y3 = x30 * sin_turn + y30 * cos_turn;
 #endif
   printf("\n\nThe base triangle angles: A = %.4fπ , B = %.4fπ , C = %.4fπ.\n\n", A/PI, B/PI, C/PI);
-  printf("The following plots show slices of the cube [0,π] x [0,π] x [0,π] whose coordinates are α, β and γ. A system\n");
-  printf("of inequalities defines an \"allowable\" portion of this cube. The slices are divided into cells. Each cell is\n");
+  printf("The following plots show slices of the cube [0,π] x [0,π] x [0,π] whose coordinates are α, β and γ. A system of\n");
+  printf("inequalities defines an \"allowable\" portion of this cube. The slices are divided into cells. Each cell is\n");
   printf("designated to be \"allowable\" or \"unallowable,\" based on the system of inequalities. However, a cell that has\n");
   printf("been designated to be \"unallowable\" might actually contain some of the allowable portion of the cube together\n");
   printf("with some of the unallowable portion of the cube, in which case calling the cell \"unallowable\" is an unfortunate\n");
@@ -158,11 +158,11 @@ int main(int argc, char **argv) {
   printf("a point P = (x, y, z) that extends the triangle ABC to form a tetrahedron ABCP. If a cell contains such a point\n");
   printf("(α, β, γ), then we call it \"occupied;\" otherwise the cell is \"unoccupied.\" (A basic understanding of the problem\n");
   printf("in the research paper that this program supports is presumed here.)\n\n");
-  printf("Each cell is represented by a character. A space character represents an unoccupied allowable cell, an \'o\'\n");
-  printf("represents an occupied allowable cell, a dot represents an unoccupied unallowable cell, and an \'x\' represents\n");
-  printf("an occupied unallowable cell. This latter case is possible since an \"unallowable\" cell might contain an allowable\n");
-  printf("portion of the cube (when it contains part of the boundary).\n\n");
-  printf("PLEASE WAIT (patience is a virtue, or use fewer points) while data is being generated (press the enter/return key if stuck) ....\n\n\n\n\n");
+  printf("Each cell is represented by a character. A space character represents an unoccupied allowable cell, an \'o\' represents\n");
+  printf("an occupied allowable cell, a dot represents an unoccupied unallowable cell, and an \'x\' represents an occupied\n");
+  printf("unallowable cell. This latter case is possible since an \"unallowable\" cell might contain an allowable portion of\n");
+  printf("the cube (when it contains part of the boundary). Ideally, there should only be cells with dots and cells with o's.\n\n");
+  printf("PLEASE WAIT (or use fewer points) while data is being generated (press the enter/return key if stuck) ....\n\n\n\n\n");
   // Use a 3D array to record possible (alpha, beta, gamma) triples for given triangle
   for (i=O; i<M-O; i++)
     for (j=O; j<M-O; j++)
